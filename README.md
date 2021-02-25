1ddc105628a9f4fab1016529114e36ee5ef43468
ZASUO01 <souzadraganeelc@gmail.com>
2021-02-24 21:02:08 -0300

    added Ai

---
+++ README.md
@@ -0,0 +1,6 @@
+# A simple browser Tic Tac Toe Game.
+
+You can play in 3 different modes:
+    Player vs Player.
+    Player vs Stupid Ia (Computer random moves)
+    Player vs Smart Ia (Computer moves by minimax Algorithm)
--- app.js
+++ app.js
@@ -1,31 +1,28 @@
-
-
 const GameBoard = (() => {
-
-    const domGameBoard = document.querySelector('#game-board');
+    
     let board_array = Array(9).fill('');
 
     const renderBoard = () => {
         const cells = document.querySelectorAll('.cell');
-        for(let i = 0; i < cells.length; i++){
-            cells[i].textContent = board_array[i];
-        }
-    }
-    
-    const markBoard = (index, marker) => {
-        if(canMark(index)){
-            board_array[index] = marker;
-        }
+        board_array.forEach((value, index) => {
+            const actCell = cells.item(index);
+            actCell.textContent = value;
+        });
     }
 
-    const canMark = (index) => {
-        if(board_array[index] != 'O' && board_array[index] != 'X') return true;
-        return false;
+
+    const markBoard = (index, marker) => {
+        if(board_array[index] != 'O' && board_array[index] != 'X') board_array[index] = marker;
     }
+
     
+
+    const  unmarkBoard = (index) => {
+        if(board_array[index] && board_array[index] != '') board_array[index] = '';
+    }
 
     const resetBoard = () => {
-        board_array = Array(9).fill('');
+       board_array = Array(9).fill(''); 
     }
 
     const getBoard = () => {
@@ -35,62 +32,70 @@ const GameBoard = (() => {
     return{
         renderBoard,
         markBoard,
+        unmarkBoard,
         resetBoard,
         getBoard
     }
+
 })();
 
 
-const Controlers = (() => {
-    const cells = document.querySelectorAll('.cell');
-
+const Controllers = (() => {
     const setWindow = document.querySelector('#set-window');
-    const gameType = document.querySelector('#game-type');
     const startBtn = document.querySelector('#start');
+    const gameTypes = document.querySelector('#game-type');
+    const cells = document.querySelectorAll('.cell');
     const resetBtn = document.querySelector('#reset');
+    let selectedType = null;
 
-    const getMode = () => {
-        const mode = gameType.value;
-        if(mode == 1) return 'stupid';
-        else if(mode == 2) return 'smart';
-        return 'normal'; 
-    }
-
-    startBtn.addEventListener('click', (ev) => {
+    startBtn.addEventListener('click',  (ev) => {
         ev.preventDefault();
         setWindow.style.display = 'none';
+        selectedType = gameTypes.value;
     });
 
     cells.forEach(cell => {
-        cell.addEventListener('click', (event) => {
-            if(event.target.textContent !== '' || Game.getGameOver() == true) return; 
+        cell.addEventListener('click', () => {
+            if(Game.getGameOver() == true) return;
+            cell.classList.add('fade-in');
             const index = Array.prototype.indexOf.call(cells, cell);
             Game.playRound(index);
-            if(getMode() == 'stupid') Game.playRound(Computer.randomIndex(GameBoard.getBoard()));
+            if(selectedType == 1 && !Game.getGameOver()){
+                const randomIndex = Computer.randomChoice();
+                cells.item(randomIndex).classList.add('fade-in');
+                Game.playRound(randomIndex);
+            }
+            else if(selectedType == 2){
+                const smartIndex = Computer.smartChoice();
+                cells.item(smartIndex).classList.add('fade-in');
+                Game.playRound(smartIndex);
+               
+            }
             GameBoard.renderBoard();
         });
-    });
+    })
 
     resetBtn.addEventListener('click', () => {
         Game.resetGame();
         GameBoard.resetBoard();
         GameBoard.renderBoard();
-        resetBtn.style.display = 'none';
+        cells.forEach(cell => {
+            cell.classList.remove('fade-in');
+        });
         setWindow.style.display = 'flex';
     });
 
-    return{
-        resetBtn
-    }
+    return{resetBtn};
+
 })();
 
 
 const Game = (() => {
-    let actRound = 1;
+    const displayH1 = document.querySelector('#game-display h1');
+    const displayP = document.querySelector('#game-display p');
+    
     let gameOver = false;
-
-    const gameDisplayH1 = document.querySelector('.game-display h1');
-    const gameDisplayP = document.querySelector('.game-display p');
+    let actTurn = 1;
 
     const winningConditions = [
         [0,1,2],
@@ -99,89 +104,157 @@ const Game = (() => {
         [0,3,6],
         [1,4,7],
         [2,5,8],
-        [0,4,8],
-        [2,4,6]
-    ];
+        [2,4,6],
+        [0,4,8]
+    ]
 
     const playRound = (index) => {
-        gameDisplayH1.textContent = `Turn: ${actRound+1}`;
-        GameBoard.markBoard(index, actMarker());
-        if(checkWin()){
-            gameDisplayP.textContent = `Player ${actMarker()} won this match!`;
-            Controlers.resetBtn.style.display = 'block';
+        displayH1.textContent = 'Turn: '+actTurn;
+        GameBoard.markBoard(index, getMarker());
+        if(checkWin(GameBoard.getBoard())){
             gameOver = true;
+            displayP.textContent = `Player ${getMarker()} has won!`;
             return;
         }
-        
-        else if(checkTie()){
-            gameDisplayP.textContent = 'This is a tie!';
-            Controlers.resetBtn.style.display = 'block';
+        if(checkTie(actTurn)){
             gameOver = true;
+            displayP.textContent = 'It\s a tie!';
             return;
         }
-        actRound++;
-        gameDisplayP.textContent = `Player ${actMarker()} turn`
+        actTurn++;
     }
 
-
-    const checkWin = () => {
+    const checkWin = (board) => {
         let isWin = false;
         winningConditions.forEach(condition => {
             const a = condition[0];
             const b = condition[1];
             const c = condition[2];
-            if(GameBoard.getBoard()[a] != '' && GameBoard.getBoard()[a] == GameBoard.getBoard()[b] && GameBoard.getBoard()[a] == GameBoard.getBoard()[c]) isWin = true;     
+            if(board[a] != '' && board[a] == board[b] && board[a] == board[c]) isWin = true;     
         });
         return isWin;
     }
 
-    const checkTie = () => {
-        if(actRound == 9) return true;
-        else return false;
+    const checkTie = (turn) => {
+        if(turn == 9) return true;
+        return false;
     }
 
-    const actMarker = () => {
-        return actRound % 2 == 0 ? 'O' : 'X';
-    }
-
-    const resetGame = () => {
-        gameOver = false;
-        actRound = 1
-        gameDisplayH1.textContent = 'Turn: 1';
-        gameDisplayP.textContent = 'Player 0 turn'
+    const getMarker = () => {
+        return actTurn%2 == 0? 'O' : 'X';
     }
 
     const getGameOver = () => {
         return gameOver;
     }
 
+    const resetGame = () => {
+        gameOver = false;
+        actTurn = 1
+        displayH1.textContent = 'Turn: 1';
+        displayP.textContent = 'Player X turn'
+    }
+
     return{
         playRound,
+        getGameOver,
         resetGame,
-        getGameOver
+        getMarker,
+        checkWin,
+        checkTie,
+        actTurn
     }
 })();
+
 
 const Computer = (() => {
-    const getFreePositions = (board) => {
-        let arr = [];
-        board.forEach((value, index) => {
-            if(value == '' && !Game.getGameOver()) arr.push(index);
+    const getFreePositions = () => {
+        let freePositions = [];
+        GameBoard.getBoard().forEach((value, index) => {
+            if(value == '') freePositions.push(index);
+        })
+        return freePositions;
+    }
+
+    const randomChoice = () => {
+        const choices = getFreePositions();
+        const size = choices.length;
+        const index = Math.floor(Math.random() * size);
+        return choices[index];
+    }
+
+    const miniMax = (board, marker) => {
+        if(Game.checkWin(board)){
+            if(marker == 'X') return 1;
+            else if(marker == 'O') return -1;
+        }
+        if(Game.checkTie(Game.actTurn)) return 0;
+        const newMarker = marker == 'O' ? 'X' : 'O';
+    
+        const possibilities = getFreePositions();
+        let bestValue = null;
+        possibilities.forEach(pos => {
+            GameBoard.markBoard(pos, newMarker);
+            Game.actTurn++;
+            const value = miniMax(GameBoard.getBoard(),newMarker);
+            Game.actTurn--;
+            GameBoard.unmarkBoard(pos);
+        
+
+            if(bestValue == null){
+                bestValue = value;
+            }
+            else if(newMarker == 'X'){
+                if(value > bestValue){
+                    bestValue = value;
+                }
+            }
+            else if(newMarker == 'O'){
+                if(value < bestValue){
+                    bestValue = value;
+                }
+            }
         });
-        return arr;
+        return bestValue;
+    }
+    
+    const smartChoice = () => {
+        const possibilities = getFreePositions();
+        let bestMovement = null;
+        let bestValue = null;
+        possibilities.forEach(pos => {
+            GameBoard.markBoard(pos, Game.getMarker());
+            Game.actTurn++;
+            const value = miniMax(GameBoard.getBoard(), Game.getMarker());
+            Game.actTurn--;
+            GameBoard.unmarkBoard(pos);
+        
+
+            if(bestValue == null){
+                bestValue = value;
+                bestMovement = pos;
+            }
+            else if(Game.getMarker() == 'X'){
+                if(value > bestValue){
+                    bestValue = value;
+                    bestMovement = pos;
+                }
+            }
+            else if(Game.getMarker() == 'O'){
+                if(value < bestValue){
+                    bestValue = value;
+                    bestMovement = pos;
+                }
+            }
+        });
+        return bestMovement;
     }
 
-    const randomIndex = (board) => {
-        const posArray = getFreePositions(board);
-        const size = posArray.length;
-        const val = Math.floor(Math.random() * size);
-        return posArray[val];
+   
+
+    return{
+        randomChoice,
+        smartChoice
     }
 
-    
-    return{randomIndex}
-    
 })();
-
-
-
